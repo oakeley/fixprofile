e54d29e7963a580b0a99b0753bc37b3efd94e087
Edward <edward.oakeley@novartis.com>
2019-06-01 23:17:00 +0200

    updated to pull glucose targets from Nightscout
    This now pulls the glucose targets from Nightscout and merges them with the new autotune data.
    According to viq none of this is needed so maybe this is of no use but hey, use it if it helps.
    It is all done in BASH so pretty easy and no real dependencies. Viq's python script depends on a few pip installs. If you get errors then try: sudo pip install texttable requests

--- fixprofile.sh
+++ fixprofile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Script to take the log file from OpenAPS autosense and reformat it into a Nightscout-compatible json for upload
-# Edward J. Oakeley, v0.1, 01-Jun-2019
+# Edward J. Oakeley, v0.2, 01-Jun-2019
 # Feel free to reuse however you like, don't even care if you claim it as your own. I give it to the community but I reserve the right to ignore requests to support it
 #WeAreNotWaiting
 # syntax: sh fixprofile.sh http://myNightscout.com myAPIsecret
+# Get the download script
+ls ~/myopenaps/nightscout/ 2> test; cat test | awk '{ if($2=="cannot") {system("mkdir ~/myopenaps/nightscout/; cd ~/myopenaps/nightscout/; wget https://raw.githubusercontent.com/viq/oref0/profile_from_nightscout/bin/get_profile.py")} }'
+rm test
 echo "{" > new.json
 echo "  \"min_5m_carbimpact\": 8.0," >> new.json
 # change "5" to whatever you use
@@ -12,8 +15,10 @@ echo "  \"basalprofile\": [" >> new.json
 # Pull all the suggestions out of the autotune log
 cat ~/myopenaps/autotune/autotune_recommendations.log | awk -F"|" '{ print $1 "[" $3 }' | tr -d " " | grep ":" | awk -F"[" -v n="0" '{ if(length($0)>6) {print "    {"; print "      \"i\": "n","; n+=1; print "      \"start\": \"" $1 ":00\","; split($1,a,":"); b=(a[1]*60)+a[2]; print "      \"minutes\": " b ","; print "      \"rate\": " $2; print "    }," } }' | sed '$ s/.$//' >> new.json
 echo "  ]," >> new.json
-# You can't upload to Nightscout without bg_targets. YOU MUST CHANGE "mmol" to "mg/dl" if you are American/German/want other units. The low/high settngs are hard coded for me. I will fix this if I can ever figure out how to download from Nightscout
-echo "" | awk '{print "  \"bg_targets\": {"; print "    \"units\": \"mmol\","; print "    \"user_preferred_units\": \"mmol\","; print "    \"targets\": ["; print "      {"; print "        \"i\": 0,"; print "        \"start\": \"00:00:00\","; print "        \"offset\": 0,"; print "        \"low\": 5,"; print "        \"min_bg\": 5,"; print "        \"high\": 8,"; print "        \"max_bg\": 8"; print "      },"; print "      {"; print "        \"i\": 1,"; print "        \"start\": \"07:00:00\","; print "        \"offset\": 25200,"; print "        \"low\": 4,"; print "        \"min_bg\": 4,"; print "        \"high\": 6.5,"; print "        \"max_bg\": 6.5"; print "      }"; print "    ]"; print "  },";}' >> new.json
+# copy the glucose targets from Nightscout. If this doesn't work for you then comment the next line and uncomment the one after
+python2 ~/myopenaps/nightscout/get_profile.py --nightscout https://oakeley.herokuapp.com display --format openaps --name 'OpenAPS Autosync' | awk -v go="True" '{ if(go=="True") {print $0; if(length($0)==7) {go="False"} } }' >> new.json
+## You can't upload to Nightscout without bg_targets. YOU MUST CHANGE "mmol" to "mg/dl" if you are American/German/want other units. The low/high settngs are hard coded for me. I will fix this if I can ever figure out how to download from Nightscout
+#echo "" | awk '{print "  \"bg_targets\": {"; print "    \"units\": \"mmol\","; print "    \"user_preferred_units\": \"mmol\","; print "    \"targets\": ["; print "      {"; print "        \"i\": 0,"; print "        \"start\": \"00:00:00\","; print "        \"offset\": 0,"; print "        \"low\": 5,"; print "        \"min_bg\": 5,"; print "        \"high\": 8,"; print "        \"max_bg\": 8"; print "      },"; print "      {"; print "        \"i\": 1,"; print "        \"start\": \"07:00:00\","; print "        \"offset\": 25200,"; print "        \"low\": 4,"; print "        \"min_bg\": 4,"; print "        \"high\": 6.5,"; print "        \"max_bg\": 6.5"; print "      }"; print "    ]"; print "  },";}' >> new.json
 echo "  \"isfProfile\": {" >> new.json
 echo "    \"sensitivities\": [" >> new.json
 echo "      {" >> new.json
